01be8391ae88128f868a7b5a6f7fe364ee002e87
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2026-06-09 17:03:55 +0800

    fixed: change the download route from general to encrypted

--- spec/integration/service_download_masked_pdf_spec.rb
+++ spec/integration/service_download_masked_pdf_spec.rb
@@ -7,33 +7,37 @@ describe 'DownloadMaskedPdf service' do
     @auth_token = 'auth-token'
     @attachment_id = 'attachment-id'
     @masked_attachment_id = 'masked-attachment-id'
-    @path = "#{API_URL}/attachments/#{@attachment_id}/masked_attachments/#{@masked_attachment_id}/download"
+    @path = "#{API_URL}/attachments/#{@attachment_id}/masked_attachments/#{@masked_attachment_id}/encrypted_download"
   end
 
   after do
     WebMock.reset!
   end
 
-  it 'HAPPY: downloads a saved masked PDF' do
-    WebMock.stub_request(:get, @path)
-           .with(headers: { 'Authorization' => "Bearer #{@auth_token}" })
+  it 'HAPPY: downloads a password-protected masked PDF' do
+    WebMock.stub_request(:post, @path)
+           .with(
+             body: { password: 'test123' }.to_json,
+             headers: { 'Authorization' => "Bearer #{@auth_token}" }
+           )
            .to_return(
              status: 200,
-             body: "%PDF-1.4\nmasked",
+             body: "%PDF-1.4\nencrypted masked",
              headers: { 'content-type' => 'application/pdf' }
            )
 
     pdf_body = LockedCV::DownloadMaskedPdf.new(app.config).call(
       attachment_id: @attachment_id,
       masked_attachment_id: @masked_attachment_id,
-      auth_token: @auth_token
+      auth_token: @auth_token,
+      password: 'test123'
     )
 
     _(pdf_body.byteslice(0, 4)).must_equal '%PDF'
   end
 
   it 'BAD: raises NotFoundError when masked attachment is missing' do
-    WebMock.stub_request(:get, @path)
+    WebMock.stub_request(:post, @path)
            .to_return(
              status: 404,
              body: { message: 'Masked attachment not found' }.to_json,
@@ -44,7 +48,8 @@ describe 'DownloadMaskedPdf service' do
       LockedCV::DownloadMaskedPdf.new(app.config).call(
         attachment_id: @attachment_id,
         masked_attachment_id: @masked_attachment_id,
-        auth_token: @auth_token
+        auth_token: @auth_token,
+        password: 'test123'
       )
     }).must_raise LockedCV::DownloadMaskedPdf::NotFoundError
   end
